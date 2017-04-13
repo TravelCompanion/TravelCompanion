ea74fb5b1a10fcf4d068ca23f75c93fecabd97cb
stanosz <stanoszstone@gmail.com>
2017-04-13 15:16:22 +0200

    début ajout de la bar de menu de la page profile

--- TravelCompanionApp/app/src/main/java/com/example/voyage/travelcompanionapp/ProfilActivity.java
+++ TravelCompanionApp/app/src/main/java/com/example/voyage/travelcompanionapp/ProfilActivity.java
@@ -1,7 +1,15 @@
 package com.example.voyage.travelcompanionapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,12 +20,19 @@ import android.widget.Toast;
 
 import com.example.voyage.travelcompanionapp.R;
 
-public class ProfilActivity extends AppCompatActivity {
+public class ProfilActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+
         TabHost host = (TabHost)findViewById(R.id.tabHost);
         host.setup();
         ListView list_avis_monument=(ListView)findViewById(R.id.list_avis_monument);
@@ -59,12 +74,21 @@ public class ProfilActivity extends AppCompatActivity {
         host.addTab(spec);*/
     }
 
-    /*@Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.layout.content_profil, menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
         return true;
-    }*/
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -79,6 +103,31 @@ public class ProfilActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_room) {
+            Intent intent = new Intent(ProfilActivity.this, MapsActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_profil) {
+            Intent intent = new Intent(ProfilActivity.this, ProfilActivity.class);
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
         /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
