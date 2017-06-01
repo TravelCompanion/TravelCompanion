8bfd96186bf4ed8bfb965e8e3a5dbe54971d9334
stanosz <stanosz.stone@gmail.com>
2017-06-01 19:10:02 +0200

    ajout d'un bouton pour recupere les elements cochés

--- TravelCompanionApp/app/src/main/java/com/example/voyage/travelcompanionapp/PreferencesActivity.java
+++ TravelCompanionApp/app/src/main/java/com/example/voyage/travelcompanionapp/PreferencesActivity.java
@@ -2,30 +2,38 @@ package com.example.voyage.travelcompanionapp;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.CheckBox;
+import android.widget.Toast;
 
-public class PreferencesActivity extends AppCompatActivity {
-    private CheckBox chkmusee;
-    private CheckBox chkmaison;
-    private CheckBox chkparc;
-    private CheckBox chkeglise;
-    private CheckBox chkjardin;
-    private CheckBox chkattraction;
-    private CheckBox chkvilla;
-    private CheckBox chkpavillon;
-    private CheckBox chkcaves;
-    private CheckBox chksocle;
-    private CheckBox chkauberge;
-    private CheckBox chkhotel;
-    private CheckBox chkimmeuble;
-    private CheckBox chkchateau;
-
+public class PreferencesActivity extends AppCompatActivity implements View.OnClickListener{
+     CheckBox chkmusee;
+     CheckBox chkmaison;
+     CheckBox chkparc;
+     CheckBox chkeglise;
+     CheckBox chkjardin;
+     CheckBox chkattraction;
+     CheckBox chkvilla;
+     CheckBox chkpavillon;
+     CheckBox chkcaves;
+     CheckBox chksocle;
+     CheckBox chkauberge;
+     CheckBox chkhotel;
+     CheckBox chkimmeuble;
+     CheckBox chkchateau;
+    String elempref="";
+    FloatingActionButton bfa;
+     String[] choose_pref=new String[14];
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
+        bfa=(FloatingActionButton)findViewById(R.id.fab_action_pref);
+
 
         chkmusee=(CheckBox) findViewById(R.id.checkBox_musee);
         chkmaison=(CheckBox) findViewById(R.id.checkBox_maison);
@@ -44,11 +52,93 @@ public class PreferencesActivity extends AppCompatActivity {
 
 
 
+        bfa.setOnClickListener(PreferencesActivity.this);
+
+
+
+
 
     }
 
     public void findidCheckbox(){
 
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        for (int i = 0; i <= 13; i++){
+            choose_pref[i] ="0";
+        }
+
+      if (v== bfa){
+
+            if (chkmusee.isChecked()) {
+                choose_pref[0] = "1";
+            }
+            if (chkeglise.isChecked()) {
+                choose_pref[1] = "1";
+            }
+            if (chkattraction.isChecked()) {
+                choose_pref[2] = "1";
+            }
+            if (chkjardin.isChecked()) {
+                choose_pref[3] = "1";
+            }
+
+            if (chkchateau.isChecked()) {
+                choose_pref[4] = "1";
+            }
+
+            if (chkparc.isChecked()) {
+                choose_pref[5] = "1";
+            }
+            if (chkimmeuble.isChecked()) {
+                choose_pref[6] = "1";
+            }
+            if (chkhotel.isChecked()) {
+                choose_pref[7] = "1";
+            }
+
+            if (chkauberge.isChecked()) {
+                choose_pref[8] = "1";
+            }
+            if (chksocle.isChecked()) {
+                choose_pref[9] = "1";
+            }
+
+            if (chkmaison.isChecked()) {
+                choose_pref[10] = "1";
+            }
+            if (chkcaves.isChecked()) {
+                choose_pref[11] = "1";
+            }
+            if (chkpavillon.isChecked()) {
+                choose_pref[12] = "1";
+            }
+            if (chkvilla.isChecked()) {
+                choose_pref[13] = "1";
+            }
+          elempref="";
+
+                for (int j = 0; j<= choose_pref.length-1; j++) {
+                if (j != 0) {
+                        elempref += choose_pref[j] + ",";
+                    }
+                }
+
+                if(elempref!=""){
+                    Log.d("listpref",elempref);
+                    Toast.makeText(PreferencesActivity.this, elempref, Toast.LENGTH_SHORT).show();
+                    //Utiliser une methode pour envoyer les elements
+                }
+                else{
+                    Log.d("listpref","pas d'element dans la liste");
+                }
+
+
+
+        }
 
     }
 }
